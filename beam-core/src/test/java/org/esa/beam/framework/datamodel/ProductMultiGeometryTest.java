d4b92849449e07c4fd95d321640e4d989b5b98cf
marpet <marco.peters@brockmann-consult.de>
2014-08-12 14:45:32 +0200

    did some naive implementation

--- beam-core/src/test/java/org/esa/beam/framework/datamodel/ProductMultiGeometryTest.java
+++ beam-core/src/test/java/org/esa/beam/framework/datamodel/ProductMultiGeometryTest.java
@@ -1,13 +1,12 @@
 package org.esa.beam.framework.datamodel;
 
 import org.geotools.referencing.crs.DefaultGeographicCRS;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.operation.TransformException;
 
 import javax.media.jai.operator.ConstantDescriptor;
+import java.awt.Dimension;
 
 import static org.junit.Assert.*;
 
@@ -25,16 +24,32 @@ public class ProductMultiGeometryTest {
 
         Band europeLR = new Band("Europe_low_res", ProductData.TYPE_INT8, 250, 550);
         europeLR.setSourceImage(ConstantDescriptor.create(250f, 550f, new Byte[]{IMAGE_CONTENT}, null));
+        // NW: -10, 65 ; SE: 15, 40
         europeLR.setGeoCoding(new CrsGeoCoding(DefaultGeographicCRS.WGS84, 250, 550, -10, 65, 0.1, 0.1));
         product.addBand(europeLR);
         Band europeHR = new Band("Europe_high_res", ProductData.TYPE_INT8, 2500, 5500);
         europeHR.setSourceImage(ConstantDescriptor.create(2500f, 5500f, new Byte[]{IMAGE_CONTENT}, null));
-        europeHR.setGeoCoding(new CrsGeoCoding(DefaultGeographicCRS.WGS84, 2500, 5500, -10, 65, 0.01, 0.01));
+        // NW: -10, 65 ; SE: 15, 40
+        CrsGeoCoding geoCoding = new CrsGeoCoding(DefaultGeographicCRS.WGS84, 2500, 5500, -10, 65, 0.01, 0.01);
+        europeHR.setGeoCoding(geoCoding);
         product.addBand(europeHR);
         Band germany = new Band("Germany", ProductData.TYPE_INT8, 2000, 2000);
-        germany.setSourceImage(ConstantDescriptor.create(2500f, 5500f, new Byte[]{IMAGE_CONTENT}, null));
-        germany.setGeoCoding(new CrsGeoCoding(DefaultGeographicCRS.WGS84, 2500, 5500, 5, 55, 0.2, 0.2));
+        germany.setSourceImage(ConstantDescriptor.create(2000f, 2000f, new Byte[]{IMAGE_CONTENT}, null));
+        // NW: 5, 55 ; SE: 15, 45
+        germany.setGeoCoding(new CrsGeoCoding(DefaultGeographicCRS.WGS84, 2000, 2000, 5, 55, 0.005, 0.005));
         product.addBand(germany);
+
+        int sceneWidth = (int) ((0.01 / 0.005) * 2500);
+        int sceneHeight = (int) ((0.01 / 0.005) * 5500);
+        Dimension sceneDimension = new Dimension(sceneWidth, sceneHeight);
+        assertEquals(sceneDimension, product.getSceneRasterSize());
+        assertEquals(sceneDimension, product.getSceneImageGeometry().getSize());
+
+        CrsGeoCoding expectedSceneGeoCoding = new CrsGeoCoding(DefaultGeographicCRS.WGS84, sceneWidth, sceneHeight, -10.05, 65.05, 0.005, 0.005, 0, 0);
+        GeoCoding actualSceneGeoCoding = product.getSceneGeoCoding();
+
+        assertEquals(expectedSceneGeoCoding.getMapCRS().toWKT(), actualSceneGeoCoding.getMapCRS().toWKT());
+        assertEquals(expectedSceneGeoCoding.getImageToMapTransform(), actualSceneGeoCoding.getImageToMapTransform());
 
     }
 }
